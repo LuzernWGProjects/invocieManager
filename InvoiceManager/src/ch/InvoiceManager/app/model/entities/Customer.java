b10b5b81ea85518dc8eeed09e044d5c8c97b00bc
Oliver <oli_fau@hotmail.com>
2014-10-10 18:41:02 +0200

    Initial commit to share first code

---
+++ InvoiceManager/src/ch/InvoiceManager/app/model/entities/Customer.java
@@ -0,0 +1,26 @@
+package ch.InvoiceManager.app.model.entities;
+
+public class Customer {
+	
+	int id;
+	String surName;
+	String firstName;
+	
+	
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	
+
+}
